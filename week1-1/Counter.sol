3a8d461af790bcd4d3890fe1f1cb3a80509375f7
Liu Zhuangzhuang <626783@gmail.com>
2023-03-08 19:56:30 +0800

    init

---
+++ week1-1/Counter.sol
@@ -0,0 +1,12 @@
+//SPDX-License-Identifier:MIT
+
+pragma solidity ^0.8.0;
+contract Counter {
+    uint256 public count;
+    constructor() {
+        count = 0;
+    }
+    function add( uint x) public  returns (uint) {
+        return count += x;
+    }
+}
